6c41a476f9d5de01c555bcff31d33c1db24ca416
Eric Joo <ericjaeminjoo@gmail.com>
2018-06-11 22:00:51 +0000

    Modified callback function to print out each avatar_url in the collection

--- avatar_downloader.js
+++ avatar_downloader.js
@@ -13,11 +13,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function (err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  
+  result.forEach(element => {
+    console.log(element.avatar_url);
+  });
 });
